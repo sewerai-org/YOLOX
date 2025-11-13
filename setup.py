144a1ebba289cbb5981f33e29bcc42af13c0ae8e
jun <t.junyong@gmail.com>
2025-11-13 14:37:55 -0800

    revert install requirements

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=("tests", "tools"))
     + list(get_package_dir().keys()),
     python_requires=">=3.6",
-    # install_requires=get_install_requirements(),
+    install_requires=get_install_requirements(),
     # setup_requires=["setuptools"],  # avoid building error when pip is not updated
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
