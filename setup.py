e6890999bb3cfb5ec64b54142178ac7c84abcadd
jun <t.junyong@gmail.com>
2025-11-13 13:02:14 -0800

    add setup tools

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setuptools.setup(
     + list(get_package_dir().keys()),
     python_requires=">=3.6",
     install_requires=get_install_requirements(),
-    setup_requires=["setuptools"],  # avoid building error when pip is not updated
+    # setup_requires=["setuptools"],  # avoid building error when pip is not updated
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     include_package_data=True,  # include files in MANIFEST.in
